40684a0ca1adacb8ec90b6f82b51fef82bb7ae97
inti-fun <sde.kapilraj@gmail.com>
2021-03-22 09:40:32 +0530

    first commit
    Revision of linkedlist

---
+++ linkedlist/singly_linkedlist.py
@@ -0,0 +1,169 @@
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+
+    def traverse(self):
+        if self.head is None:
+            print("Empty")
+            return
+        cnode = self.head
+        res = ""
+        while cnode:
+            res += str(cnode.data) + " > "
+            cnode = cnode.next
+        print(res)
+        return
+
+    def searching(self, ele):
+        if self.head is None:
+            print("Empty")
+            return
+        index = 0
+        cnode = self.head
+        while cnode:
+            if cnode.data == ele:
+                print(f"{ele} found at {index}")
+                return
+            cnode = cnode.next
+            index += 1
+        else:
+            print(f"{ele} is not present in the list")
+            return
+
+    def ref_to_last_node(self):
+        if self.head is None:
+            print("empty")
+            return
+        cnode = self.head
+        while cnode.next:
+            cnode = cnode.next
+        print(f"Last node is {cnode.data}")
+        return cnode
+
+    def ref_to_second_last(self):
+        if self.head is None:
+            print("Empty")
+            return
+        cnode = self.head
+        while cnode.next.next:
+            cnode = cnode.next
+        print("Second last node is ", cnode.data)
+        return cnode
+
+    def ref_to_node_data(self, ele):
+        if self.head is None:
+            print("empty")
+            return
+        cnode = self.head
+        index = 0
+        while cnode:
+            if cnode.data == ele:
+                break
+            index += 1
+            cnode = cnode.next
+        else:
+            print(f"{ele} not found")
+            return
+        print(f"Ref found for {ele} at index {index}")
+        return
+
+    def ref_to_predNode(self, ele):
+        if self.head is None:
+            print("Empty")
+            return
+        cnode = self.head
+        if cnode.data == ele:
+            print("Node present at 0th index")
+            return None
+        while cnode.next:
+            if cnode.next.data == ele:
+                print(f"Predecessor of {ele} is {cnode.data}")
+                return cnode
+            cnode = cnode.next
+        else:
+            print(f"{ele} is not present in the list")
+        return
+
+    def ref_to_node_pos(self, pos):
+        if self.head is None:
+            print("Empty")
+            return
+        cnode = self.head
+        index = 0
+        while cnode and index <= pos:
+            if index == pos:
+                print(f"{cnode.data}")
+                return cnode
+            index += 1
+            cnode = cnode.next
+        else:
+            print(f"Index {pos} > len of list")
+
+    def insert_at_start(self, data):
+        if self.head is None:
+            self.head = Node(data)
+            return self.head
+
+        cnode = self.head
+        new_node = Node(data, cnode)
+        self.head = new_node
+        return self.head
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data)
+            return self.head
+        cnode = self.head
+        while cnode.next:
+            cnode = cnode.next
+        cnode.next = Node(data)
+        return
+
+    def insert_at_pos(self, pos, data):
+        if self.head is None and pos == 0:
+            self.head = Node(data)
+            return self.head
+        elif pos < 0:
+            print("-ve index")
+            return
+        cnode = self.head
+        index = 0
+        while cnode and index <= pos:
+            if index == pos:
+                new_node = Node(data)
+                new_node.next = cnode.next
+                cnode = new_node
+                return
+            cnode = cnode.next
+        else:
+            print(f"{pos} excedded the total length")
+            return
+
+
+l = Linkedlist()
+l.insert_at_start(10)
+l.insert_at_start(30)
+l.insert_at_start(50)
+l.insert_at_start(60)
+l.insert_at_start(70)
+# l.traverse()
+# l.searching(50)
+
+# l.ref_to_last_node()
+# l.ref_to_second_last()
+# l.ref_to_node_data(50)
+# l.ref_to_predNode(50)
+# l.ref_to_node_pos(4)
+
+# l.traverse()
+l.insert_at_end(50)
+l.insert_at_end(450)
+l.traverse()
+l.insert_at_pos(1, 0)
+l.traverse()
--- linkedlist/singlylinkedlist.py
+++ linkedlist/singlylinkedlist.py
@@ -8,6 +8,15 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def traverse(self):
+        if self.head is None:
+            print("No node")
+            return
+        cnode = self.head
+        while cnode:
+            print(f"{cnode.data} ->")
+            cnode = cnode.next
+
     def display(self):
         if self.head is None:
             print("List is empty")
@@ -19,19 +28,6 @@ class LinkedList:
             cnode = cnode.next
         print(res)
 
-    def insert_at_begining(self, data):
-        self.head = Node(data, self.head)
-
-    def insert_at_end(self, data):
-        if self.head is None:
-            self.head = Node(data, self.head)
-            return
-        cnode = self.head
-        while cnode.next:
-            cnode = cnode.next
-        cnode.next = Node(data, None)
-        return
-
     def search(self, data):
         if self.head is None:
             print("List is empty/ Not found")
@@ -40,22 +36,10 @@ class LinkedList:
         index = 0
         while cnode:
             if cnode.data == data:
-                print(f"Found at index {index}")
+                print(f"Element {data} found at index {index}")
                 break
             cnode = cnode.next
             index += 1
-
-    def insert_at_pos(self, data, pos):
-        if self.head is None and pos == 1:
-            self.head = Node(data, self.head)
-            return
-
-    def del_from_begining(self):
-        if self.head is None:
-            print("No node")
-            return
-        self.head = self.head.next
-        return self.head
 
     def ref_to_last_node(self):
         if self.head is None:
@@ -84,34 +68,184 @@ class LinkedList:
         index = 0
         while cnode is not None:
             if cnode.data == serach_this:
-                print(f"found at {index}")
-                break
+                print(f"found at index {index}")
+                return
             cnode = cnode.next
             index += 1
         else:
             print("Not present in the list")
 
+    def ref_to_predescessor(self, pred_of_this):
+        if self.head is None:
+            print("No node is present in the list")
+            return
+        cnode = self.head
+        while cnode.next is not None:
+            if cnode.next.data == pred_of_this:
+                print(cnode.data)
+                return
+            cnode = cnode.next
+        else:
+            print("No pred for this node. returning none")
+            return None
 
+    def ref_to_particular_pos(self, pos=None, data=None):
+        if self.head is None:
+            print("List is empty")
+            return
+        cnode = self.head
+        index = 0
+        while cnode is not None and index <= pos:
+            if index == pos or cnode.data == data:
+                print(f"found at index {index}")
+                return
+            cnode = cnode.next
+            index += 1
+        else:
+            print(f"{pos} related error.")
+            return
+
+    def insert_at_begining(self, data):
+        self.head = Node(data, self.head)
+
+    def insert_after_this(self, old_node, data):
+        if self.head is None:
+            print("No node")
+            return
+        cnode = self.head
+        while cnode.next:
+            if cnode.data == old_node:
+                new_node = Node(data)
+                new_node.next = cnode.next
+                cnode.next = new_node
+                return
+            cnode = cnode.next
+        else:
+            print("Node is not present in the list")
+            return
+
+    def insert_before_this(self, old_node, data):
+        if self.head is None:
+            print("No node")
+            return
+        cnode = self.head
+        prev_node = None
+        if cnode.data == old_node:
+            new_node = Node(data)
+            new_node.next = cnode
+            cnode = new_node
+            return
+        while cnode is not None:
+            if cnode.data == old_node:
+                new_node = Node(data)
+                new_node.next = cnode
+                prev_node.next = new_node
+                return
+            prev_node = cnode
+            cnode = cnode.next
+        else:
+            print(f"Didn't found {old_node} in the list")
+            return
+
+    def insert_at_pos(self, pos, data):
+        if pos == 1:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+            return
+        cnode = self.head
+        index = 0
+        while cnode is not None and index < pos - 1:
+            cnode = cnode.next
+            index += 1
+
+        if cnode is None:
+            print("you can only insert node at pos ", pos)
+        else:
+            new_node = Node(data)
+            new_node.next = cnode.next
+            cnode.next = new_node
+            return
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, self.head)
+            return
+        cnode = self.head
+        while cnode.next:
+            cnode = cnode.next
+        cnode.next = Node(data, None)
+        return
+
+    def insert_in_empty_list(self, data):
+        self.head = Node(data=new_data)
+        return self.head
+
+    def del_from_begining(self):
+        if self.head is None:
+            print("No node")
+            return
+        elif self.head.next is None:
+            print("Deleting the only node")
+            self.head = None
+            return
+        else:
+            self.head = self.head.next
+            return self.head
+
+    def del_last_node(self):
+        if self.head is None:
+            print("zero node")
+            return
+        elif self.head.next is None:
+            self.head = None
+            return
+        else:
+            cnode = self.head
+            while cnode.next:
+                cnode = cnode.next
+            cnode = None
+            return
+
+
+# working methods
+# myLinkedList = LinkedList()
+# myLinkedList.insert_at_begining(10)
+# myLinkedList.insert_at_begining(4)
+# myLinkedList.insert_at_end(170)
+# myLinkedList.search(10)
+# myLinkedList.del_from_begining()
+# myLinkedList.ref_to_last_node()
+# myLinkedList.ref_to_second_last()
+# myLinkedList.ref_to_a_specific_node(5)
+# myLinkedList.ref_to_predescessor(7)
+# myLinkedList.ref_to_particular_pos(5)
+
+# print("-------------------")
+# myLinkedList.display()
+# print("-------------------")
+
+# a new list
 myLinkedList = LinkedList()
+myLinkedList.insert_at_end(0)
+myLinkedList.insert_at_end(1)
+myLinkedList.insert_at_end(2)
+myLinkedList.insert_at_end(3)
+myLinkedList.insert_at_end(4)
+myLinkedList.insert_at_end(5)
+myLinkedList.insert_at_end(6)
+myLinkedList.insert_at_end(7)
+
+myLinkedList.insert_after_this(4, 44)
 # myLinkedList.display()
-myLinkedList.insert_at_begining(10)
+myLinkedList.insert_before_this(4, 33)
 # myLinkedList.display()
-myLinkedList.insert_at_begining(4)
+myLinkedList.insert_at_pos(2, 5)
 # myLinkedList.display()
-myLinkedList.insert_at_end(170)
+print("-------------------")
 myLinkedList.display()
-myLinkedList.search(10)
-myLinkedList.display()
+print("-------------------")
 myLinkedList.del_from_begining()
-myLinkedList.ref_to_last_node()
-myLinkedList.ref_to_second_last()
-# recreating the list
-
-myLinkedList.insert_at_begining(130)
-# myLinkedList.display()
-myLinkedList.insert_at_begining(44)
-# myLinkedList.display()
-myLinkedList.insert_at_end(15)
-
 myLinkedList.display()
-myLinkedList.ref_to_a_specific_node(18)
+myLinkedList.del_last_node()
+myLinkedList.display()
